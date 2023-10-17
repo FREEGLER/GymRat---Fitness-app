c81e1215e6bb1d2a022c2f58f80a6d443451df63
FREEGLER <70941029+FREEGLER@users.noreply.github.com>
2023-10-17 03:13:26 +0200

    Prototype is gemaakt

--- GymRat - Fitness-app/Form1.Designer.cs
+++ GymRat - Fitness-app/Form1.Designer.cs
@@ -29,12 +29,154 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.flypSidebar = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblMenu = new System.Windows.Forms.Label();
+            this.menuButton = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnGoals = new System.Windows.Forms.Button();
+            this.sidebarTimer = new System.Windows.Forms.Timer(this.components);
+            this.flypSidebar.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.menuButton)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flypSidebar
+            // 
+            this.flypSidebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.flypSidebar.Controls.Add(this.panel1);
+            this.flypSidebar.Controls.Add(this.panel3);
+            this.flypSidebar.Controls.Add(this.panel2);
+            this.flypSidebar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flypSidebar.Location = new System.Drawing.Point(0, 0);
+            this.flypSidebar.MaximumSize = new System.Drawing.Size(300, 680);
+            this.flypSidebar.MinimumSize = new System.Drawing.Size(65, 680);
+            this.flypSidebar.Name = "flypSidebar";
+            this.flypSidebar.Size = new System.Drawing.Size(65, 680);
+            this.flypSidebar.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblMenu);
+            this.panel1.Controls.Add(this.menuButton);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(297, 118);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblMenu
+            // 
+            this.lblMenu.AutoSize = true;
+            this.lblMenu.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblMenu.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMenu.ForeColor = System.Drawing.Color.White;
+            this.lblMenu.Location = new System.Drawing.Point(107, 17);
+            this.lblMenu.Name = "lblMenu";
+            this.lblMenu.Size = new System.Drawing.Size(73, 28);
+            this.lblMenu.TabIndex = 1;
+            this.lblMenu.Text = "MENU";
+            // 
+            // menuButton
+            // 
+            this.menuButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menuButton.Image = ((System.Drawing.Image)(resources.GetObject("menuButton.Image")));
+            this.menuButton.Location = new System.Drawing.Point(7, 7);
+            this.menuButton.Name = "menuButton";
+            this.menuButton.Size = new System.Drawing.Size(43, 40);
+            this.menuButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.menuButton.TabIndex = 0;
+            this.menuButton.TabStop = false;
+            this.menuButton.Click += new System.EventHandler(this.menuButton_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Location = new System.Drawing.Point(3, 127);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(297, 50);
+            this.panel3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(-19, -4);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(25, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(337, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "                  PROFILE";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnGoals);
+            this.panel2.Location = new System.Drawing.Point(3, 183);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(297, 50);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnGoals
+            // 
+            this.btnGoals.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGoals.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGoals.ForeColor = System.Drawing.Color.White;
+            this.btnGoals.Image = ((System.Drawing.Image)(resources.GetObject("btnGoals.Image")));
+            this.btnGoals.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGoals.Location = new System.Drawing.Point(-19, -4);
+            this.btnGoals.Name = "btnGoals";
+            this.btnGoals.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.btnGoals.Size = new System.Drawing.Size(337, 59);
+            this.btnGoals.TabIndex = 0;
+            this.btnGoals.Text = "                   GOALS";
+            this.btnGoals.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGoals.UseVisualStyleBackColor = true;
+            this.btnGoals.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sidebarTimer
+            // 
+            this.sidebarTimer.Interval = 10;
+            this.sidebarTimer.Tick += new System.EventHandler(this.sidebarTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(54)))), ((int)(((byte)(54)))));
+            this.ClientSize = new System.Drawing.Size(380, 680);
+            this.Controls.Add(this.flypSidebar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.flypSidebar.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.menuButton)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button btnGoals;
+        private System.Windows.Forms.FlowLayoutPanel flypSidebar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox menuButton;
+        private System.Windows.Forms.Label lblMenu;
+        private System.Windows.Forms.Timer sidebarTimer;
     }
 }
 
--- GymRat - Fitness-app/Form1.cs
+++ GymRat - Fitness-app/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,53 @@ namespace GymRat___Fitness_app
 {
     public partial class Form1 : Form
     {
+        bool sidebarExpand;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sidebarTimer_Tick(object sender, EventArgs e)
+        {
+            if (sidebarExpand) 
+            {
+                flypSidebar.Width -= 10;
+                if (flypSidebar.Width == flypSidebar.MinimumSize.Width)
+                {
+                    sidebarExpand = false;
+                    sidebarTimer.Stop();
+                }
+            }
+            else
+            {
+                flypSidebar.Width += 10;
+                if (flypSidebar.Width == flypSidebar.MaximumSize.Width)
+                {
+                    sidebarExpand = true;
+                    sidebarTimer.Stop();
+                }
+            }
+
+        }
+
+        private void menuButton_Click(object sender, EventArgs e)
+        {
+            sidebarTimer.Start();
+        }
+
+        
     }
 }
